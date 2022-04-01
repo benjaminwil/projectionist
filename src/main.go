af61fb1dcb73314defde9e6ca175fd5bd3a3548d
benjamin wil <benjamin@super.gd>
2022-04-01 09:29:58 -0700

    Read `./.projections.json` file
    This commit allows the program to read a `.projections.json` file in the
    current directory. A step in the right direction, but borderline
    useless.
    
    I also changed the name of the acceptance test file.

--- src/main.go
+++ src/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
   "fmt"
+  "io/ioutil"
   "os"
 )
 
 func main() {
-  fmt.Println(ExitMessage())
-  os.Exit(0)
+  projections, err := ReadProjections(ioutil.ReadFile)
+
+  if err == nil {
+    fmt.Println(projections)
+    os.Exit(0)
+  } else {
+    fmt.Println(err)
+    os.Exit(1)
+  }
 }
 
-func ExitMessage() string {
-  return "ok"
+func ReadProjections(readfile func(string) ([]byte, error)) (string, error) {
+  if contents, err := readfile("./.projections.json"); err == nil {
+    return string(contents), nil
+  } else {
+    return "no file", err
+  }
 }
--- src/main_test.go
+++ src/main_test.go
@@ -1,11 +1,43 @@
 package main
 
-import "testing"
+import (
+  "bytes"
+  "errors"
+  "fmt"
+  "io/ioutil"
+  "testing"
+)
 
-func TestExitMessage(t *testing.T) {
-  result := ExitMessage()
+type DummyReadFile struct {
+  Str string
+}
 
-  if result != "ok" {
-    t.Errorf("function should return 'ok' but got %s", result)
+func (f DummyReadFile) ReadFile(filename string) ([]byte, error) {
+  buffer := bytes.NewBufferString(f.Str)
+  return ioutil.ReadAll(buffer)
+}
+
+func TestReadProjection_NoFilePresent(t *testing.T) {
+  expectedError := fmt.Errorf("no such file")
+  noFileToRead := func(filename string) ([]byte, error) {
+    return make([]byte, 0, 48), expectedError
+  }
+  result, err := ReadProjections(noFileToRead)
+
+  if result != "no file" {
+    t.Errorf("Function should return 'no file' but returned %s", result)
+  }
+
+  if !errors.Is(err, expectedError)  {
+    t.Errorf("Expected a 'no such file' error but didn't get one.")
+  }
+}
+
+func TestReadProjections_FilePresent(t *testing.T) {
+  dummy_ioutil := DummyReadFile{Str: "{}"}
+  result, _ := ReadProjections(dummy_ioutil.ReadFile)
+
+  if result != "{}" {
+    t.Errorf("function should return '{}' but returned %s", result)
   }
 }
