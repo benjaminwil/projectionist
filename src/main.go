4a8fdd6c549dc7ca050f38cc56a5a777c3418219
benjamin wil <benjamin@super.gd>
2022-04-02 13:00:00 -0700

    Run `go fmt`
    Sorry I'm still learning.

--- src/main.go
+++ src/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-  projections, err := ReadProjections(ioutil.ReadFile)
+	projections, err := ReadProjections(ioutil.ReadFile)
 
-  if err == nil {
-    fmt.Println(projections)
-    os.Exit(0)
-  } else {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err == nil {
+		fmt.Println(projections)
+		os.Exit(0)
+	} else {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func ReadProjections(readfile func(string) ([]byte, error)) (string, error) {
-  if contents, err := readfile("./.projections.json"); err == nil {
-    return string(contents), nil
-  } else {
-    return "no file", err
-  }
+	if contents, err := readfile("./.projections.json"); err == nil {
+		return string(contents), nil
+	} else {
+		return "no file", err
+	}
 }
--- src/main_test.go
+++ src/main_test.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-  "bytes"
-  "errors"
-  "fmt"
-  "io/ioutil"
-  "testing"
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"testing"
 )
 
 type DummyReadFile struct {
-  Str string
+	Str string
 }
 
 func (f DummyReadFile) ReadFile(filename string) ([]byte, error) {
-  buffer := bytes.NewBufferString(f.Str)
-  return ioutil.ReadAll(buffer)
+	buffer := bytes.NewBufferString(f.Str)
+	return ioutil.ReadAll(buffer)
 }
 
 func TestReadProjection_NoFilePresent(t *testing.T) {
-  expectedError := fmt.Errorf("no such file")
-  noFileToRead := func(filename string) ([]byte, error) {
-    return make([]byte, 0, 48), expectedError
-  }
-  result, err := ReadProjections(noFileToRead)
+	expectedError := fmt.Errorf("no such file")
+	noFileToRead := func(filename string) ([]byte, error) {
+		return make([]byte, 0, 48), expectedError
+	}
+	result, err := ReadProjections(noFileToRead)
 
-  if result != "no file" {
-    t.Errorf("Function should return 'no file' but returned %s", result)
-  }
+	if result != "no file" {
+		t.Errorf("Function should return 'no file' but returned %s", result)
+	}
 
-  if !errors.Is(err, expectedError)  {
-    t.Errorf("Expected a 'no such file' error but didn't get one.")
-  }
+	if !errors.Is(err, expectedError) {
+		t.Errorf("Expected a 'no such file' error but didn't get one.")
+	}
 }
 
 func TestReadProjections_FilePresent(t *testing.T) {
-  dummy_ioutil := DummyReadFile{Str: "{}"}
-  result, _ := ReadProjections(dummy_ioutil.ReadFile)
+	dummy_ioutil := DummyReadFile{Str: "{}"}
+	result, _ := ReadProjections(dummy_ioutil.ReadFile)
 
-  if result != "{}" {
-    t.Errorf("function should return '{}' but returned %s", result)
-  }
+	if result != "{}" {
+		t.Errorf("function should return '{}' but returned %s", result)
+	}
 }
