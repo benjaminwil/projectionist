95376b14d11ae844b38d84b91a8aad2931f9052f
benjamin wil <benjamin@super.gd>
2022-04-17 15:20:33 -0700

    Reorder `main` package funcs and add code comments
    Just cleanin' up. No functional changes.

--- src/main.go
+++ src/main.go
@@ -19,19 +19,60 @@ var (
 )
 
 func main() {
+	// Before processing user-provided arguments, configure flags and subcommands
+	// accepted by the the commandline interface.
 	configureFlags()
+
+	// If no arguments, or invalid arguments, are given, exit early.
 	exitIfInvalidArguments()
 
-	arguments := niceargs.List(os.Args[2:])
-
+	// Process the given subcommand, as well as any flags and positional
+	// arguments.
 	command := subcommands[os.Args[1]]
-	command.Parse(arguments)
+	command.Parse(niceargs.List(os.Args[2:]))
+
+	// Read the given .projections.json file so it can be processed by a
+	// subcommand.
 	data := readProjections()
 
+	// If the user didn't provide a final argument (for getting a file alternate
+	// back), then exit.
 	exitIfNoFilesGiven(command)
 
-	printAlternates(data, command)
+	// Process subcommand. Right now the only available subcommand is "alternate".
+	alternates(data, command)
 
+	// We should have exited already. But if not, exit with an error message.
+	exitIfFin()
+}
+
+func alternates(data map[string]interface{}, command *flag.FlagSet) {
+	if command != alternateCommand {
+		return
+	}
+
+	results, err := projections.AlternateOf(data, command.Args()[0])
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(results)
+	os.Exit(0)
+}
+
+func configureFlags() {
+	for _, flagSet := range subcommands {
+		config = flagSet.String(
+			"config",
+			fmt.Sprintf("%s/.projections.json", pwd()),
+			"Provide a JSON file to read projections from.",
+		)
+	}
+}
+
+func exitIfFin() {
 	fmt.Println(`Something went wrong.
               If you received this, you may want to submit a bug report.`)
 	os.Exit(1)
@@ -48,40 +89,6 @@ func exitIfNoFilesGiven(command *flag.FlagSet) {
 	if command.NArg() == 0 {
 		fmt.Println("No files given.")
 		os.Exit(1)
-	}
-}
-
-func printAlternates(data map[string]interface{}, command *flag.FlagSet) {
-	if os.Args[1] == "alternate" {
-		results, err := projections.AlternateOf(data, command.Args()[0])
-
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		fmt.Println(results)
-		os.Exit(0)
-	}
-}
-
-func subcommandList() string {
-	list := make([]string, 0, len(subcommands))
-
-	for name := range subcommands {
-		list = append(list, name)
-	}
-
-	return strings.Join(list, ", ")
-}
-
-func configureFlags() {
-	for _, flagSet := range subcommands {
-		config = flagSet.String(
-			"config",
-			fmt.Sprintf("%s/.projections.json", pwd()),
-			"Provide a JSON file to read projections from.",
-		)
 	}
 }
 
@@ -104,4 +111,14 @@ func readProjections() map[string]interface{} {
 	}
 
 	return projections
+}
+
+func subcommandList() string {
+	list := make([]string, 0, len(subcommands))
+
+	for name := range subcommands {
+		list = append(list, name)
+	}
+
+	return strings.Join(list, ", ")
 }
